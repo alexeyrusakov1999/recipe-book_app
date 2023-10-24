65feb4d0020302d7cdb01fd95155a3f772413bf9
Alexey Rusakov <alexeyrusakov1999@gmail.com>
2023-10-25 02:34:54 +0300

    feat: header and recipe item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,24 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./components/header/header.component";
-import { RecipesComponent } from './pages/recipe-book/recipes/recipes.component';
-import { RecipesListComponent } from './pages/recipe-book/recipes/recipes-list/recipes-list.component';
-import { RecipeDetailComponent } from './pages/recipe-book/recipes/recipe-detail/recipe-detail.component';
-import { ReipeItemComponent } from './pages/recipe-book/recipes/recipes-list/reipe-item/reipe-item.component';
-import { ShoppingListComponent } from './pages/shopping-list/shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './pages/shopping-list/shopping-list/shopping-edit/shopping-edit.component';
+import { RecipesComponent } from "./pages/recipe-book/recipes/recipes.component";
+import { RecipesListComponent } from "./pages/recipe-book/recipes/recipes-list/recipes-list.component";
+import { RecipeDetailComponent } from "./pages/recipe-book/recipes/recipe-detail/recipe-detail.component";
+import { RecipeItemComponent } from "./pages/recipe-book/recipes/recipes-list/recipe-item/recipe-item.component";
+import { ShoppingListComponent } from "./pages/shopping-list/shopping-list/shopping-list.component";
+import { ShoppingEditComponent } from "./pages/shopping-list/shopping-list/shopping-edit/shopping-edit.component";
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, RecipesComponent, RecipesListComponent, RecipeDetailComponent, ReipeItemComponent, ShoppingListComponent, ShoppingEditComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipesListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/pages/recipe-book/recipes/recipes-list/recipes-list.component.ts
+++ src/app/pages/recipe-book/recipes/recipes-list/recipes-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Recipe } from "../recipe.model";
 
 @Component({
-  selector: 'app-recipes-list',
-  templateUrl: './recipes-list.component.html',
-  styleUrls: ['./recipes-list.component.css']
+  selector: "app-recipes-list",
+  templateUrl: "./recipes-list.component.html",
+  styleUrls: ["./recipes-list.component.css"],
 })
 export class RecipesListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      "Pasta",
+      "make a pasta with love",
+      "https://s1.eda.ru/StaticContent/Photos/121017153819/190315152129/p_O.jpg"
+    ),
+    new Recipe(
+      "Pasta",
+      "make a pasta with love",
+      "https://s1.eda.ru/StaticContent/Photos/121017153819/190315152129/p_O.jpg"
+    ),
+  ];
 }
