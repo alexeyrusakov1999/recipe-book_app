c922aa277d3f93cc42728072e5070916c900f35f
Alexey Rusakov <alexeyrusakov1999@gmail.com>
2023-10-29 00:13:59 +0300

    feat: starting set up

--- src/app/pages/shopping-list/shopping-list/shopping-list.component.ts
+++ src/app/pages/shopping-list/shopping-list/shopping-list.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Ingredient } from "src/app/shared/ingredient.model";
 
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  selector: "app-shopping-list",
+  templateUrl: "./shopping-list.component.html",
+  styleUrls: ["./shopping-list.component.css"],
 })
 export class ShoppingListComponent {
-
+  ingredients: Ingredient[] = [
+    new Ingredient("Apples", 5),
+    new Ingredient("Potatoes", 8),
+  ];
 }
