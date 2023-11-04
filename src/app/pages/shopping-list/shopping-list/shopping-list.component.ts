8023408663698039afcbdd67ac1ca52082cc8326
Alexey Rusakov <alexeyrusakov1999@gmail.com>
2023-11-05 02:39:19 +0300

    feat: add simle addin to shopping-list

--- src/app/pages/shopping-list/shopping-list/shopping-list.component.ts
+++ src/app/pages/shopping-list/shopping-list/shopping-list.component.ts
@@ -11,4 +11,9 @@ export class ShoppingListComponent {
     new Ingredient("Apples", 5),
     new Ingredient("Potatoes", 8),
   ];
+
+  onIngredientAdded(ingredient: Ingredient) {
+    event.preventDefault();
+    this.ingredients.push(ingredient);
+  }
 }
