38bec7495cbb6ad37e13c5c52afc3eaebb04b166
Alexey Rusakov <alexeyrusakov1999@gmail.com>
2023-11-05 01:44:01 +0300

    feat: components binding and simple routing

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,14 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
   styleUrls: ["./header.component.css"],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @Output() selectedFeature = new EventEmitter<string>();
+
+  onSelected(selectedPage: string) {
+    this.selectedFeature.emit(selectedPage);
+  }
+}
--- src/app/pages/recipe-book/recipes/recipes-list/recipes-list.component.ts
+++ src/app/pages/recipe-book/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 import { Recipe } from "../recipe.model";
 
 @Component({
@@ -14,9 +14,15 @@ export class RecipesListComponent {
       "https://s1.eda.ru/StaticContent/Photos/121017153819/190315152129/p_O.jpg"
     ),
     new Recipe(
-      "Pasta",
-      "make a pasta with love",
+      "Pasta-2",
+      "make a pasta with love and sex",
       "https://s1.eda.ru/StaticContent/Photos/121017153819/190315152129/p_O.jpg"
     ),
   ];
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
+  onRecipesNavigate(recipe: Recipe) {
+    console.log(recipe);
+    this.recipeWasSelected.emit(recipe);
+  }
 }
